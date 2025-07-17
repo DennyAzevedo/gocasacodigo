80db497efca86e45e1712baef0fa9e51058693c0
Denny Azevedo <denny.filho@animaeducacao.com.br>
2025-07-16 21:11:10 -0300

    Arrays e matrizes

--- Iniciando/arrays.go
+++ Iniciando/arrays.go
@@ -8,5 +8,23 @@ func main() {
 	primos := [...]int{2, 3, 5, 7, 11, 13}
 	nomes := [2]string{}
 
-	fmt.Println(a, numeros, primos, nomes)
+	fmt.Printf("Arrays - a: %v, numeros: %v, primos: %v, nomes: %v\n",
+		a,
+		numeros,
+		primos,
+		nomes,
+	)
+	fmt.Printf("Tamanho dos Arrays - a: %d, numeros: %d, primos: %d, nomes: %d\n",
+		len(a),
+		len(numeros),
+		len(primos),
+		len(nomes),
+	)
+
+	var multiA[2][2]int
+	multiA[0][0], multiA[0][1] = 3, 5
+	multiA[1][0], multiA[1][1] = 7, -2
+
+	multiB := [2][2]int{{2, 13}, {-1, 6}}
+	fmt.Printf("Matrizes:\nmultiA: %v\nmultiB: %v\n", multiA, multiB)
 }
